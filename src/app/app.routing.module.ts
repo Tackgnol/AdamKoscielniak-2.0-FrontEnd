b615407b758c3739b1146d8b81a9721be06e568e
Adam Kościelniak <koscielniakadam@gmail.com>
2021-03-02 19:33:35 +0100

    Fixed token validation

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginFormComponent } from './modules/Login/components/login-form/login-form.component';
+import { AdminComponent } from './modules/Admin/admin.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginFormComponent },
+  {path: 'admin', component: AdminComponent, can},
   { path: '', component: LoginFormComponent }
 ];
 @NgModule({
